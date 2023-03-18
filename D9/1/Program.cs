80949362ef02ad0200272ecbde3a5b335a44b3b5
Artom Va <a6660778@gmail.com>
2023-03-18 15:43:04 +0300

    Выходные

---
+++ D9/1/Program.cs
@@ -0,0 +1,88 @@
+﻿interface Ix
+{
+    void IxF0(int w);
+    void IxF1();
+}
+
+interface Iy
+{
+    void F0(int w);
+    void F1();
+}
+
+interface Iz
+{
+    void F0(int w);
+    void F1();
+}
+
+class TestClass : Ix, Iy, Iz
+{
+    public int w;
+
+    // Неявная реализация методов Iy
+    public void F0(int w)
+    {
+        Console.WriteLine("Результат неявной реализации метода F0 интерфейса Iy: " + w * 3);
+    }
+
+    public void F1()
+    {
+        Console.WriteLine("Метод F1 интерфейса Iy выполнен");
+    }
+
+    // Явная реализация методов Iz
+    void Iz.F0(int w)
+    {
+        Console.WriteLine("Результат явной реализации метода F0 интерфейса Iz: " + (6 + w));
+    }
+
+    void Iz.F1()
+    {
+        Console.WriteLine("Метод F1 интерфейса Iz выполнен");
+    }
+
+    public void IxF0(int w)
+    {
+        Console.WriteLine("Результат метода IxF0 интерфейса Ix: " + (7 * w - 4));
+    }
+
+    public void IxF1()
+    {
+        Console.WriteLine("Метод IxF1 интерфейса Ix выполнен");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TestClass test = new TestClass();
+        test.w = 5;
+
+        // Вызов методов интерфейса Iy
+        ((Iy)test).F0(test.w);
+        ((Iy)test).F1();
+
+        // Вызов методов интерфейса Iz
+        ((Iz)test).F0(test.w);
+        ((Iz)test).F1();
+
+        // Вызов методов интерфейса Ix
+        test.IxF0(test.w);
+        test.IxF1();
+
+        // Вызов методов через интерфейсную ссылку параметр
+        Ix ix = test;
+        ix.IxF0(test.w);
+        ix.IxF1();
+
+        Iy iy = test;
+        iy.F0(test.w);
+        iy.F1();
+
+        Iz iz = test;
+        iz.F0(test.w);
+        iz.F1();
+    }
+}
---
+++ D9/2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+// Класс "Книга"
+class Book : IComparable<Book>
+{
+    // Поля книги
+    public string title;
+    public string author;
+    public int year;
+
+    // Конструктор книги
+    public Book(string title, string author, int year)
+    {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    // Реализация интерфейса IComparable для сравнения по году издания
+    public int CompareTo(Book other)
+    {
+        return this.year.CompareTo(other.year);
+    }
+}
+
+// Класс "Домашняя библиотека"
+class HomeLibrary
+{
+    // Массив книг
+    private Book[] books;
+
+    // Конструктор библиотеки
+    public HomeLibrary()
+    {
+        books = new Book[0];
+    }
+
+    // Добавление книги в библиотеку
+    public void AddBook(Book book)
+    {
+        Array.Resize(ref books, books.Length + 1);
+        books[books.Length - 1] = book;
+    }
+
+    // Удаление книги из библиотеки
+    public void RemoveBook(int index)
+    {
+        for (int i = index; i < books.Length - 1; i++)
+        {
+            books[i] = books[i + 1];
+        }
+        Array.Resize(ref books, books.Length - 1);
+    }
+
+    // Поиск книги по году издания
+    public Book[] FindBooksByYear(int year)
+    {
+        Book[] result = new Book[0];
+        foreach (Book book in books)
+        {
+            if (book.year == year)
+            {
+                Array.Resize(ref result, result.Length + 1);
+                result[result.Length - 1] = book;
+            }
+        }
+        return result;
+    }
+
+    // Получение книги по номеру
+    public Book GetBook(int index)
+    {
+        return books[index];
+    }
+}
+
+// Точка входа в программу
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание библиотеки и добавление книг
+        HomeLibrary library = new HomeLibrary();
+        library.AddBook(new Book("Жизнь в коде", "Варченя", 1866));
+        library.AddBook(new Book("Мастер и Маргарита", "М.А. Булгаков", 1966));
+        library.AddBook(new Book("11/22/63", "Стивен Кинг", 2011));
+        library.AddBook(new Book("Унесенные ветром", "М.Митчелл", 1936));
+
+        // Поиск книг по году издания
+        Console.Write("Введите год издания книги, которые хотите найти: ");
+        int year = int.Parse(Console.ReadLine());
+        Book[] booksByYear = library.FindBooksByYear(year);
+        if (booksByYear.Length == 0)
+        {
+            Console.WriteLine("Книги, изданные в {0} году, не найдены.", year);
+        }
+        else
+        {
+            Console.WriteLine("Книги, изданные в {0} году:", year);
+            foreach (Book book in booksByYear)
+            {
+                Console.WriteLine(book.title + " от " + book.author);
+            }
+        }
+
+        // Удаление книги из библиотеки
+        Console.Write("Введите номер книги, которую хотите удалить: ");
+        int bookIndex = int.Parse(Console.ReadLine());
+        library.RemoveBook(bookIndex);
+
+        // Получение книги по номеру
+        Console.Write("Введите номер книги, которую хотите получить: ");
+        int bookNumber = int.Parse(Console.ReadLine());
+        Book bookByNumber = library.GetBook(bookNumber);
+        Console.WriteLine("Книга под номером {0}: {1}", bookNumber, bookByNumber.title);
+    }
+}
