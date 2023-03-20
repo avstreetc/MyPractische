b76d88626b4626759dd6109e0077101afe875124
Artom Va <a6660778@gmail.com>
2023-03-20 12:31:36 +0300

    ха

--- D12/1/Program.cs
+++ D12/1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿CalcFigure cf = GetLength;
+cf += GetArea;
+cf += GetVolume;
+Console.Write("Введите значение R: ");
+int R = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Результат выполнения делегата: {cf(R):.###}");
+
+static double GetLength(double R) => 2 * Math.PI * R;
+static double GetArea(double R) => Math.PI * R * R;
+static double GetVolume(double R) => 4 / 3 * Math.PI * R * R * R;
+
+delegate double CalcFigure(double R);
