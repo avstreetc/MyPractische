5ed4dc5939719ce7b951ab352e04b410f7ec5f5e
Artom Va <a6660778@gmail.com>
2023-03-25 12:45:30 +0300

    Солнечный день

---
+++ D15/1/Program.cs
@@ -0,0 +1,61 @@
+﻿
+static class MyListExtensions
+{
+    public static T[] GetArray<T>(this MyList<T> list)
+    {
+        T[] array = new T[list.Count];
+        for (int i = 0; i < list.Count; i++)
+        {
+            array[i] = list[i];
+        }
+        return array;
+    }
+}
+
+class MyList<T>
+{
+    private List<T> list;
+
+    // Конструктор класса MyList, инициализирующий новый экземпляр списка List<T> 
+    public MyList()
+    {
+        list = new List<T>();
+    }
+
+    // доавляем новый элемент в список list 
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+    // индексатор для получение элемента
+    public T this[int index]
+    {
+        get { return list[index]; }
+    }
+
+    //общее количество элементов
+    public int Count
+    {
+        get { return list.Count; }
+    }
+}
+
+class a1
+{
+    static void Main(string[] args)
+    {
+        MyList<int> list = new MyList<int>(); // экземпляр класса 
+
+        list.Add(10);
+        list.Add(20); // добавляем элементы int ибо int
+        list.Add(30);
+
+        int[] array = list.GetArray();
+        foreach (int item in array)
+        {
+            Console.WriteLine("Элементы массива, возвращаемые GetArray: " + item);
+        }
+        Console.WriteLine("Количество элементов в списке: " + list.Count);
+    }
+}
---
+++ D15/2/Program.cs
@@ -0,0 +1,53 @@
+﻿MyDictionary<string, int> dictionary = new MyDictionary<string, int>();
+dictionary.Add("one", 1);
+dictionary.Add("two", 2);
+dictionary.Add("three", 3);
+Console.WriteLine(dictionary["two"]); // Выведет 2
+Console.WriteLine(dictionary.Count); // Выведет 3
+
+
+class MyDictionary<TKey, TValue>
+{
+    private List<TKey> keys; // список ключей
+    private List<TValue> values; // список значений
+
+    // Конструктор класса MyDictionary, инициализирующий новые экземпляры списков ключей и значений
+    public MyDictionary()
+    {
+        keys = new List<TKey>();
+        values = new List<TValue>();
+    }
+
+    // Метод добавления пары элементов
+    public void Add(TKey key, TValue value)
+    {
+        keys.Add(key);
+        values.Add(value);
+    }
+
+    // Индексатор для получения значения элемента по указанному индексу
+    public TValue this[TKey key]
+    {
+        get
+        {
+            int index = keys.IndexOf(key);
+            if (index == -1)
+                throw new KeyNotFoundException(); // генерируем исключение, если ключ не найден
+            return values[index];
+        }
+        set
+        {
+            int index = keys.IndexOf(key);
+            if (index == -1)
+                throw new KeyNotFoundException(); // генерируем исключение, если ключ не найден
+            values[index] = value;
+        }
+    }
+
+    // Свойство только для чтения для получения общего количества пар элементов
+    public int Count
+    {
+        get { return keys.Count; }
+    }
+}
+
